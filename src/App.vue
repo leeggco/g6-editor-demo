156b8a00eaa0ca9a726b20e7d8f70c4f9fb721ac
leeggco <leeggco@gmail.com>
2019-03-15 18:08:00 +0800

    'update'

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,20 @@
           <el-col :span="4">
             <!-- 元素面板栏 -->
             <div id="itempannel" class="ph left">
-              <div class="getItem" data-type="node" data-shape="flow-rect" data-size="120*48" data-color="#1890FF">
-                <img draggable="false" src="./assets/graph1.png" alt="" srcset="">
+              <div class="getItem" data-type="node" data-shape="flow-rect" data-size="120*48" data-label="常规节点" data-color="#1890FF">
+                <img draggable="false" src="https://gw.alipayobjects.com/zos/rmsportal/wHcJakkCXDrUUlNkNzSy.svg"  alt="" srcset="">
               </div>
-              <div class="getItem" data-type="node" data-shape="flow-capsule" data-size="80*48" data-color="#FD0DFF">
-                <img draggable="false" src="./assets/graph2.png" alt="" srcset="">
+              <div class="getItem" data-type="node" data-shape="flow-circle" data-size="72*72" data-label="起止节点" data-color="#FA8C16">
+                <img draggable="false" src="https://gw.alipayobjects.com/zos/rmsportal/ZnPxbVjKYADMYxkTQXRi.svg"  alt="" srcset="">
+              </div>
+              <div class="getItem" data-type="node" data-shape="flow-rhombus" data-size="80*72" data-label="分叉节点" data-color="#13C2C2">
+                <img draggable="false" src="https://gw.alipayobjects.com/zos/rmsportal/SnWIktArriZRWdGCnGfK.svg"  alt="" srcset="">
+              </div>
+              <div class="getItem" data-type="node" data-shape="flow-capsule" data-size="80*48" data-label="模型节点" data-color="#722ED1">
+                <img draggable="false" src="https://gw.alipayobjects.com/zos/rmsportal/rQMUhHHSqwYsPwjXxcfP.svg"  alt="" srcset="">
+              </div>
+              <div class="getItem" data-type="node" data-shape="customNode" data-size="80*48" data-label="我是自定义的" data-color="#722ED1">
+                <img draggable="false" src="https://user-gold-cdn.xitu.io/2019/3/15/169809645b016da6?w=114&h=128&f=png&s=1893"  alt="" srcset="">
               </div>
             </div>
           </el-col>
@@ -32,7 +41,7 @@
               <!-- 详细面板 -->
               <div id="detailpannel" class="detailpannel">
                 <div data-status="node-selected" class="panel" id="node_detailpanel">
-                  <div class="panel-title">模型详情</div>
+                  <div class="panel-title">属性详情</div>
                   <div class="block-container">
                     <el-input v-model="nodeLabel" size="mini" @change="changeNodeLabel" placeholder="请输入内容"></el-input>
                   </div>
@@ -51,7 +60,6 @@
   </el-container>
 </template>
 <script>
-  import G6 from '@antv/g6'
   import G6Editor from '@antv/g6-editor'
   export default {
     data () {
@@ -60,98 +68,8 @@
         nodeLabel: ''
       }
     },
-    mounted () {
-      // this
-      const _this = this
-      // G6Editor
-      const editor = new G6Editor()
-      // editor挂载到Vue
-      this.editor = editor
-      // Command
-      const Command = G6Editor.Command
-
-      // 主画布
-      // Mind 思维导图
-      // Koni 网络图、拓扑图
-      // Flow 流程图
-      const page = new G6Editor.Flow({
-        graph: {
-          container: 'page'
-        }
-      })
-
-      // 设置边样式，内置3种样式
-      // flow-polyline
-      // flow-polyline-round
-      // flow-smooth
-      page.getGraph().edge({
-        shape: 'flow-polyline'
-      })
-
-      // 元素面板栏
-      const itempannel = new G6Editor.Itempannel({
-        container: 'itempannel',
-      })
-
-      // 工具栏
-      const toolbar = new G6Editor.Toolbar({
-        container: 'toolbar',
-      })
-
-      // 详细面板
-      const detailpannel = new G6Editor.Detailpannel ({
-          container : 'detailpannel'
-      })
-
-      // 缩略图
-      const minimap = new G6Editor.Minimap({
-        container: 'minimap',
-        height: 226,
-        width: 226,
-        curZoom: .5
-      })
-
-      // 组件挂载到Editor
-      editor.add(page)
-      editor.add(itempannel)
-      editor.add(toolbar)
-      editor.add(detailpannel)
-      editor.add(minimap)
-
-      // 获取当前page
-      const currentPage = editor.getCurrentPage()
-      // 监听选择变化
-      currentPage.on('afteritemselected', ev => {
-        // 选择对象为Node节点
-        if (ev.item.isNode) {
-          // 获取属性
-          const nm = ev.item.getModel()
-          _this.nodeLabel = nm.label
-        }
-
-        // 选择对象为Edge节点
-        if (ev.item.isEdge) {
-          // 获取属性
-          const nm = ev.item.getModel()
-          _this.nodeLabel = nm.label
-        }
-      })
-
-      // 保存命令
-      Command.registerCommand('save', {
-        // 命令是否进入队列，默认是 true
-        queue: false,  
-        // 命令是否可用
-        enable(eidtor) {
-          return true
-        },
-        // 正向命令
-        execute(eidtor) {
-          _this.saveGraph()
-        },
-        // 快捷键：Ctrl+shirt+s
-        // shortcutCodes : [['ctrlKey', 'shiftKey', 's']]
-      })
+    mounted() {
+      this.initG6Editor()
     },
     methods: {
       // 画布保存方法
@@ -160,7 +78,6 @@
         const page = this.editor.getCurrentPage()
         // 保存画布
         const data = page.save()
-        console.log(data)
       },
       // 修改Node的label
       changeNodeLabel(value) {
@@ -174,10 +91,140 @@
             })
           })
         })
+      },
+      // 初始化
+      initG6Editor() {
+        // this
+        const _this = this
+        // G6Editor
+        const editor = new G6Editor()
+        // editor挂载到Vue
+        this.editor = editor
+        // Flow
+        const Flow = G6Editor.Flow
+        // Command
+        const Command = G6Editor.Command
+        
+        // 定义Save命令
+        Command.registerCommand('save', {
+          // 命令是否进入队列，默认是 true
+          queue: false,  
+          // 命令是否可用
+          enable(eidtor) {
+            return true
+          },
+          // 正向命令
+          execute(eidtor) {
+            _this.saveGraph()
+          },
+          // 快捷键：Ctrl+shirt+s
+          shortcutCodes : [['ctrlKey', 'shiftKey', 's']]
+        })
+
+        // 主画布
+        // Mind 思维导图
+        // Koni 网络图、拓扑图
+        // Flow 流程图
+        const page = new G6Editor.Flow({
+          graph: {
+            container: 'page'
+          }
+        })
+
+        // 设置边样式，内置3种样式
+        // flow-polylinels
+        // flow-polyline-round
+        // flow-smooth
+        page.getGraph().edge({
+          shape: 'flow-polyline'
+        })
+
+        // 自定义节点
+        Flow.registerNode('customNode', {
+          draw(item){
+            const group = item.getGraphicGroup()
+            const model = item.getModel()
+            group.addShape('text', {
+              attrs: {
+                x: 0,
+                y: 0,
+                fill: '#333',
+                text: model.label
+              }
+            })
+            group.addShape('text', {
+              attrs: {
+                x: 0,
+                y: 0,
+                fill: '#333',
+                text: ' ('+model.x+', '+model.y+') \n 原点是组的图坐标',
+                textBaseline: 'top'
+              }
+            })
+            
+            return group.addShape('rect', {
+              attrs: {
+                x: 0,
+                y: 0,
+                width: 100,
+                height: 100,
+                stroke: 'red'
+              }
+            })
+          }
+        })
+
+        // 元素面板栏
+        const itempannel = new G6Editor.Itempannel({
+          container: 'itempannel',
+        })
+
+        // 工具栏
+        const toolbar = new G6Editor.Toolbar({
+          container: 'toolbar',
+        })
+
+        // 详细面板
+        const detailpannel = new G6Editor.Detailpannel ({
+          container: 'detailpannel'
+        })
+
+        // 缩略图
+        const minimap = new G6Editor.Minimap({
+          container: 'minimap',
+          height: 226,
+          width: 226
+        })
+        // 组件挂载到Editor
+        editor.add(page)
+        editor.add(itempannel)
+        editor.add(toolbar)
+        editor.add(detailpannel)
+        editor.add(minimap)
+
+        // 获取当前page
+        const currentPage = editor.getCurrentPage()
+        // 监听选择变化
+        currentPage.on('afteritemselected', ev => {
+          // 选择对象为Node节点
+          if (ev.item.isNode) {
+            // 获取属性
+            const nm = ev.item.getModel()
+            _this.nodeLabel = nm.label
+          }
+
+          // 选择对象为Edge节点
+          if (ev.item.isEdge) {
+            // 获取属性
+            const nm = ev.item.getModel()
+            _this.nodeLabel = nm.label
+          }
+        })
       }
     },
   }
 </script>
+
 <style>
   .wrap {
     border: 1px solid #eee;
@@ -201,6 +248,15 @@
   .left {
     border-right: 1px solid #eee;
     background: #fcfcfc;
+  }
+  .left .getItem {
+    float: left;
+    width: 100px;
+    height: 100px;
+    margin-left: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
   .right {
     border-left: 1px solid #eee;
